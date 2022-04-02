97098de9a39c08fb1502af3f13d741d5bc10a567
naidu <papinaidu887@gmail.com>
2022-04-03 00:23:27 +0530

    E323232101

--- src/main/java/com/medhansh/TestDemo.java
+++ src/main/java/com/medhansh/TestDemo.java
@@ -7,6 +7,14 @@ public class TestDemo {
 		System.out.println("add method");
 	}
 	
+	public static void add1()
+	{
+		System.out.println("add1()=======================================");
+	}
+	{
+		
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Main method.............");
 		

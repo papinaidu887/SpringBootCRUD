381107b03f4b9d84ce8ae319ad24d5cd082dee80
naidu <papinaidu887@gmail.com>
2022-04-02 19:01:39 +0530

    Bug fix : IES-6678

--- src/main/java/com/medhansh/Restapi1Application.java
+++ src/main/java/com/medhansh/Restapi1Application.java
@@ -8,6 +8,14 @@ public class Restapi1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Restapi1Application.class, args);
+		
+		System.out.println("hdlahfls");
+		
+		System.out.println("morning......");
+		
+		System.out.println("Hello.............");
+		
+		System.out.println("sadfafsa");
 	}
 
 }
